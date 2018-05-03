e9f4179852f1ced161f3969173aaba72af575729
Danny <dzmen@hotmail.com>
2018-05-04 00:00:56 +0200

    Delete plugins because DMCA

---
+++ Meccanoid-android/Assets/Scripts/ScreenScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenScript : MonoBehaviour {
+
+    public Canvas MainCanvas;
+    public Canvas CommandCanvas;
+
+    // This is the first script that is loaded when enabling the app. 
+    void Start () {
+        MainCanvas.enabled = true;
+        CommandCanvas.enabled = false;
+    }
+	
+}
